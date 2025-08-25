303b527abedc06b7c25294aa4e19277641e42568
sbsaville <92499716+sbsaville@users.noreply.github.com>
2025-08-25 15:27:50 -0500

    removed a useless comment

--- src/macros.h
+++ src/macros.h
@@ -88,8 +88,8 @@ void initializeMacros() {
   macroManager.addMacro(APOSTROPHE_LL,    "'ll");
 
   ADD_ALTCODE(KEY_DEGREES,    KEYPAD_PLUS, KEYPAD_0, KEYPAD_0, KEY_B, KEYPAD_0);
-  ADD_ALTCODE(PLUS_MINUS,     KEYPAD_PLUS, KEYPAD_0, KEYPAD_0, KEY_B, KEYPAD_1);
   ADD_ALTCODE(E_MACRON,       KEYPAD_PLUS, KEYPAD_0, KEYPAD_1, KEYPAD_1, KEYPAD_3);
+  ADD_ALTCODE(PLUS_MINUS,     KEYPAD_PLUS, KEYPAD_0, KEYPAD_0, KEY_B, KEYPAD_1);
   ADD_ALTCODE(GREAT_EQUAL,    KEYPAD_2, KEYPAD_4, KEYPAD_2);
   ADD_ALTCODE(LESS_EQUAL,     KEYPAD_2, KEYPAD_4, KEYPAD_3);
   ADD_ALTCODE(NOT_EQUAL,      KEYPAD_PLUS, KEYPAD_2, KEYPAD_2, KEYPAD_6, KEYPAD_0);
@@ -97,4 +97,3 @@ void initializeMacros() {
 }
 
 #endif
-// I'm building a keyboard with only 12 columns so some sacrifices have to be made, and I've chosen to remove the apostrophe/quote key. I'm practicing without it and I've come to realize that putting it on another layer is fine until I'm trying to type words with apostrophes in fast bursts. To get some speed back, I'm configuring macros to give me letters with apostrophes that commonly follow them. So far, I've configured "'s", "'t", "'m", and "'ve". What am I missing?
