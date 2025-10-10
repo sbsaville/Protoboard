a94ee32e198923175057e15fb52589e45d754dfc
sbsaville <92499716+sbsaville@users.noreply.github.com>
2025-10-10 17:10:14 -0500

    little merge fix

--- src/layers.h
+++ src/layers.h
@@ -187,8 +187,8 @@ KeyMapEntry layer4DT[rowsCount][columnsCount] = {
 
 // intentionally unused
 KeyMapEntry layoutNUL[rowsCount][columnsCount] = {
-  //  ESC       F1        F2        F3        F4        F5        F6        F7        F8        F9        F10       F11       F12       DEL      
-  {{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  },{  NUL  }},
+  /*   ESC         F1          F2          F3          F4          F5          F6          F7          F8          F9          F10         F11         F12         DEL   */
+  {{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   }},
 
   /*    ~           1           2           3           4           5           6           7           8           9           0           -           =          Home  */
   {{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   },{   NUL   }},
